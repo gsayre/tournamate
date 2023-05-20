e9d3e97273caf4fe4e44f5d6df126294f955e322
Grant Sayre <grant@grants-air.lan>
2023-05-20 09:56:30 -0400

    working on game view, and make stuff db reliant

--- src/pages/dashboard/admin/[tournamentId]/[gameId].tsx
+++ src/pages/dashboard/admin/[tournamentId]/[gameId].tsx
@@ -1,8 +1,32 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function RefView() {
+  const router = useRouter();
+
   return (
     <div className="flex h-screen w-screen">
-      <p>Reffing View</p>
+      <button onClick={() => {router.back()}}>Go Back</button>
+      <ScoreCard score={0} playerOneName={"Player One"} playerTwoName={"Player Two"} />
+      <ScoreCard score={0} playerOneName={"Player Three"} playerTwoName={"Player Four"} />
     </div>
   );
+}
+
+type ScoreCardProps = {
+  score: number;
+  playerOneName: string;
+  playerTwoName: string;
+};
+
+function ScoreCard({score, playerOneName, playerTwoName}: ScoreCardProps) {
+  return (
+    <div>
+      <div className="flex flex-col"> 
+        <p>{score}</p>
+        <p>{playerOneName}</p>
+        <p>{playerTwoName}</p>
+      </div>
+    </div>
+  )
 }
