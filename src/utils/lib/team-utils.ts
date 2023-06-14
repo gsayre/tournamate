412a12ccda2fe55e0f0d70e0471f4bf3eb08a5f4
Grant Sayre <grant@Grants-MacBook-Air.local>
2023-06-14 12:49:09 -0500

    removed example router

--- src/utils/lib/team-utils.ts
+++ src/utils/lib/team-utils.ts
@@ -58,83 +58,83 @@ export const createPoolsFromEntries = (
     return returnArr;
   };
   
-  export const createFakeEntriesAnyTeams = (numTeams: number): FakeEntriesTeamArr => {
-    const fakeEntries: FakeEntriesTeamArr = [];
-    for (let i = 0; i < numTeams; i++) {
-      fakeEntries.push(
-        createFakeTeam({
-          teamId: i + 1,
-          divisionId: 1,
-          teamRating: Math.floor(Math.random() * 1500),
-          tournamentId: 1,
-          poolId: "1",
-          userOneId: Math.floor(Math.random() * 1000).toString(),
-          userTwoId: Math.floor(Math.random() * 1000).toString(),
-          userOneName: `Boy ${i}`,
-          userTwoName:  `Girl ${i}`,
-        })
-      );
-    }
-    return fakeEntries;
-  };
-  
-  
-  type FakeTeamCriteria = {
-    teamId: number;
-    divisionId: number;
-    teamRating: number;
-    tournamentId: number;
-    poolId: string;
-    userOneId: string;
-    userTwoId: string;
-    userOneName: string;
-    userTwoName: string;
-  };
-  export function createFakeTeam({
+export const createFakeEntriesAnyTeams = (numTeams: number): FakeEntriesTeamArr => {
+  const fakeEntries: FakeEntriesTeamArr = [];
+  for (let i = 0; i < numTeams; i++) {
+    fakeEntries.push(
+      createFakeTeam({
+        teamId: i + 1,
+        divisionId: 1,
+        teamRating: Math.floor(Math.random() * 1500),
+        tournamentId: 1,
+        poolId: "1",
+        userOneId: Math.floor(Math.random() * 1000).toString(),
+        userTwoId: Math.floor(Math.random() * 1000).toString(),
+        userOneName: `Boy ${i}`,
+        userTwoName:  `Girl ${i}`,
+      })
+    );
+  }
+  return fakeEntries;
+};
+
+type FakeTeamCriteria = {
+  teamId: number;
+  divisionId: number;
+  teamRating: number;
+  tournamentId: number;
+  poolId: string;
+  userOneId: string;
+  userTwoId: string;
+  userOneName: string;
+  userTwoName: string;
+};
+
+export function createFakeTeam({
+  teamId,
+  teamRating,
+  divisionId,
+  tournamentId,
+  poolId,
+  userOneId,
+  userTwoId,
+  userOneName,
+  userTwoName,
+}: FakeTeamCriteria): FakeEntriesTeam {
+  const fakeTeam = {
     teamId,
-    teamRating,
     divisionId,
     tournamentId,
+    teamRating,
     poolId,
-    userOneId,
-    userTwoId,
-    userOneName,
-    userTwoName,
-  }: FakeTeamCriteria): FakeEntriesTeam {
-    const fakeTeam = {
-      teamId,
-      divisionId,
-      tournamentId,
-      teamRating,
-      poolId,
-      poolWins: 0,
-      poolLosses: 0,
-      players: [
-        {
-          userId: userOneId,
-          teamId,
-          user: {
-            id: userOneId,
-            fullName: userOneName,
-            isAdmin: false,
-            isTournamentDirector: false,
-            playerRating: Math.floor(Math.random() * 1500),
-          },
+    poolWins: 0,
+    poolLosses: 0,
+    players: [
+      {
+        userId: userOneId,
+        teamId,
+        user: {
+          id: userOneId,
+          fullName: userOneName,
+          isAdmin: false,
+          isTournamentDirector: false,
+          playerRating: Math.floor(Math.random() * 1500),
         },
-        {
-          userId: userTwoId,
-          teamId,
-          user: {
-            id: userTwoId,
-            fullName: userTwoName,
-            isAdmin: false,
-            isTournamentDirector: false,
-            playerRating: Math.floor(Math.random() * 1500),
-          },
+      },
+      {
+        userId: userTwoId,
+        teamId,
+        user: {
+          id: userTwoId,
+          fullName: userTwoName,
+          isAdmin: false,
+          isTournamentDirector: false,
+          playerRating: Math.floor(Math.random() * 1500),
         },
-      ],
-    };
-    return fakeTeam;
-  }
+      },
+    ],
+  };
+  return fakeTeam;
+}
   
   
